eac185465bef0438d781ec5ba7aa6b9617a4a747
Cooper Sullivan <thecoopersully@gmail.com>
2022-10-15 20:26:38 -0500

    Add notes to sqlite_manager.py

--- sqlite_manager.py
+++ sqlite_manager.py
@@ -1,10 +1,13 @@
 import os
 import sqlite3
 
-conn: sqlite3.Connection = None
-curr: sqlite3.Cursor = None
+conn: sqlite3.Connection
+curr: sqlite3.Cursor
 
 
+# Connects to the pre-existing database in the set directory.
+# If the database does not exist in the set directory, one is
+# created and then a connection is established.
 def create_or_connect():
     global conn, curr
     did_exist = False
@@ -18,10 +21,14 @@ def create_or_connect():
         create_table()
 
 
+# Creates the 'messages' table
 def create_table():
     curr.execute("CREATE TABLE messages (uid integer, exported smallint, scanned smallint)")
 
 
+# Accepts values for a complete entry in the 'messages' table.
+# If an entry with the given UID already exists, it is updated to
+# reflect the given values. If one does not exist, it is created.
 def log(uid: int, exported_bool: bool, scanned_bool: bool):
     if exists(uid):
         log_update(uid, exported_bool, scanned_bool)
@@ -29,8 +36,8 @@ def log(uid: int, exported_bool: bool, scanned_bool: bool):
         log_new(uid, exported_bool, scanned_bool)
 
 
+# Manually updates an entry in the 'messages' table.
 def log_update(uid: int, exported_bool: bool, scanned_bool: bool):
-
     exported = bool_to_int(exported_bool)
     scanned = bool_to_int(scanned_bool)
 
@@ -41,8 +48,9 @@ def log_update(uid: int, exported_bool: bool, scanned_bool: bool):
         )
 
 
+# Manually logs a new entry into the 'messages' table.
+# WARNING! CAN CAUSE DUPLICATES. Use #log() instead!
 def log_new(uid: int, exported_bool: bool, scanned_bool: bool):
-
     exported = bool_to_int(exported_bool)
     scanned = bool_to_int(scanned_bool)
 
@@ -53,11 +61,14 @@ def log_new(uid: int, exported_bool: bool, scanned_bool: bool):
         )
 
 
+# Queries if an entry with the given UID exists. Returns a boolean value.
 def exists(uid: int):
-    curr.execute("SELECT * FROM messages WHERE uid=:uid", {'uid': uid})
+    with conn:
+        curr.execute("SELECT * FROM messages WHERE uid=:uid", {'uid': uid})
     return curr.fetchone() is not None
 
 
+# Queries if an entry has been exported. Returns a boolean value.
 def is_exported(uid: int):
     curr.execute("SELECT exported FROM messages WHERE uid=:uid", {'uid': uid})
     result = curr.fetchone()
@@ -66,6 +77,7 @@ def is_exported(uid: int):
     return int_to_bool(result[0])
 
 
+# Marks an entry as exported in the 'messages' table
 def export(uid: int):
     if not exists(uid):
         log_new(uid, True, False)
@@ -75,6 +87,7 @@ def export(uid: int):
         print(f'Logged export of {uid}')
 
 
+# Queries if an entry has been scanned. Returns a boolean value.
 def is_scanned(uid: int):
     curr.execute("SELECT scanned FROM messages WHERE uid=:uid", {'uid': uid})
     result = curr.fetchone()
@@ -83,33 +96,45 @@ def is_scanned(uid: int):
     return int_to_bool(result[0])
 
 
+# Marks an entry as scanned in the 'messages' table
 def scan(uid: int):
     if not exists(uid):
         log_new(uid, False, True)
     else:
         with conn:
             curr.execute("UPDATE messages SET scanned = 1 WHERE uid = :uid", {'uid': uid})
-        print(f'Logged scanning of { uid }')
+        print(f'Logged scanning of {uid}')
 
 
+# Executes an SQL statement from the current Cursor
 def execute_sql(command):
     with conn:
         curr.execute(command)
-        return str(curr.fetchone())
+    return str(curr.fetchone())
 
 
+# Converts an integer to a boolean value
 def int_to_bool(value: int):
-    return value == 1
+    return value > 0
 
 
+# Converts a boolean value to an integer
 def bool_to_int(value: bool):
     if value:
         return 1
     return 0
 
 
+# Fetches the email in the database with the highest UID
+# that has been exported and scanned; returns its uid (int).
+def get_latest_email():
+    with conn:
+        curr.execute("SELECT max(uid) FROM messages WHERE exported = 1 AND scanned = 1")
+    return curr.fetchone()
+
+
 if __name__ == "__main__":
     create_or_connect()
     log(-1, True, False)
 
-    print(f'Exported? { is_exported(-1) }, Scanned? { is_scanned(-1) }')
+    print(f'Exported? {is_exported(-1)}, Scanned? {is_scanned(-1)}')
