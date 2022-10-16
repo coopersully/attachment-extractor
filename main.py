35e8dc01de1d858e956325844bae624b73347ad6
Cooper Sullivan <thecoopersully@gmail.com>
2022-10-15 20:46:31 -0500

    Improve error catching

--- main.py
+++ main.py
@@ -32,18 +32,22 @@ def fetch_login():
 
         # Fetch input_folder directory from file
         # If the directory doesn't exist, create it
-        input_folder = login_json['directory_raw']
+        input_folder = login_json['input_directory']
         if not os.path.isdir(input_folder):
+            print(f'Input directory "{ input_folder } does not exist; creating it now."')
             os.makedirs(input_folder, exist_ok=True)
 
         # Fetch output_folder directory from file
         # If the directory doesn't exist, create it
-        output_folder = login_json['directory_scanned']
+        output_folder = login_json['output_directory']
         if not os.path.isdir(output_folder):
+            print(f'Output directory "{ output_folder } does not exist; creating it now."')
             os.makedirs(output_folder, exist_ok=True)
 
     except KeyError as e:
+        print()
         print(f'Error loading username, password, or directory; please provide all fields in login.json')
+        print(f'Cause of exception: { e }')
         sys.exit()
 
 
